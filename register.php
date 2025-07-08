e2458fddbf8f1d66bbd41171a957f6e68331ea0a
gutti-misdo <2301162@s.asojuku.ac.jp>
2025-07-09 00:10:48 +0900

    create/user-register-page

---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ユーザ登録画面</title>
+</head>
+<body>
+    <h1>ログイン</h1>
+    <form action="check.php" method="post">
+    <p>ユーザ名：<input type="text" name="user"></p>
+    <p>パスワード：<input type="password" name="password"></p>
+    <p><button type="submit">登録</button></p>
+    <p><a href="login.php">ログインはこちら</a></p>
+    </form>
+</body>
+</html>
